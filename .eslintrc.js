e0a80b3170dee285374d1629d76884b10ce68676
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2024-02-01 23:51:54 +0300

    webpack config

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'google',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['github'],
+  root: true,
+  rules: {
+    'no-multiple-empty-lines': [1, {'max': 1, 'maxEOF': 0}],
+    'semi': 'off',
+    'linebreak-style': 0,
+    'arrow-parens': 'off',
+    'comma-dangle': 'off',
+    'require-jsdoc': 'off',
+    '@typescript-eslint/no-var-requires': 0,
+    'operator-linebreak': 'off',
+    'no-explicit-any': 'off',
+    'max-len': 'off'
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const filename = (ext) => isDev ? `bundle.${ext}` : `bundle.[chunkhash].${ext}`;
+
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.ts',
+  devtool: isDev ? 'inline-source-map' : false,
+  devServer: {
+    port: 3000,
+    hot: isDev,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [
+      new TsconfigPathsPlugin()
+    ],
+  },
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '/src/public/**/*'), 
+          to: path.resolve(__dirname, 'dist')
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+    new ESLintPlugin({
+      extensions: ['ts'],
+    }),
+  ],
+};
