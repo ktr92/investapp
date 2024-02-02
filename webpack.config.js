c7934730b220af94c8b22dd720c06d433d60c095
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2024-02-02 21:36:28 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -71,8 +71,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, '/src/public/**/*'), 
-          to: path.resolve(__dirname, 'dist')
+          from: path.resolve(__dirname, 'src/public')
         },
       ],
     }),
