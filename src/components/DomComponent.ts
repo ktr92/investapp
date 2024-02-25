0af6912c8a1476c095a3abfeedf6759ae79f1b8d
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2024-02-25 14:51:15 +0300

    create double sort

--- src/components/DomComponent.ts
+++ src/components/DomComponent.ts
@@ -1,4 +1,15 @@
 export abstract class DomComponent {
+  $(selector: string) {
+    const $el = document.querySelectorAll(selector)
+    if ($el) {
+      if ($el.length === 1) {
+        return $el[0]
+      } else {
+        return $el
+      }
+    }
+  }
+
   toggleClass(element: HTMLElement, selector: string, className: string) {
     const oldElement = document.querySelector(selector)
     if (oldElement) {
---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+const state = {
+  portfolio: [
+    {
+      id: 1,
+      name: 'SBER',
+      depo: 450000,
+      comm: 0.06
+    },
+    {
+      id: 2,
+
+    }
+  ]
+}
+
+const getters = {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
