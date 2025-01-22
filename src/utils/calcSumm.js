4f9690b6430af78c08e1a7d94b9bd84772d9936c
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2025-01-22 22:22:00 +0300

    view update

--- src/utils/calcSumm.js
+++ src/utils/calcSumm.js
@@ -3,9 +3,7 @@ import numberWithSpaces from './formatNumber'
 export default function calcSumm(source, ...keys) {
   let totalCount = 0
   source.forEach(item => {
-    /* totalCount += item[fields[0]][fields[1]] */
     totalCount += keys.reduce((prev, current) => {
-      console.log(item[current])
       return prev[current]
     }, item)
   })
