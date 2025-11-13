0439b1ae5983fe3eccfd8d19388036b64e363d97
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2025-11-13 15:29:52 +0300

    deploy github

--- src/components/table/TableTotal.ts
+++ src/components/table/TableTotal.ts
@@ -12,7 +12,7 @@ export class TableTotal extends TableComponent {
           <span class="w-auto block mr-3">
           <span class="block font-medium text-gray-900 whitespace-nowrap dark:text-white">${this.price.value ? this.price.value + '₽' : ''}</span>          </span>
           <span>
-            <span class="block  bg-primary-100 text-slate-400 text-xs font-medium rounded dark:bg-primary-900 dark:text-primary-300">${this.price.single} ₽</span>
+            <span class="block  bg-primary-100 text-slate-400 text-xs font-medium rounded dark:bg-primary-900 dark:text-primary-300">${this.price.single ? this.price.single + '₽' : ''} ₽</span>
             </span>  
       </div>
     `
